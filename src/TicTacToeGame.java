d51f31858b8c60b1c6cffb7fd25c2112bee3afc7
Richa Sondhi <richa.sondhi@ibm.com>
2021-11-14 21:22:03 +0100

    Tic Tac Toe Game test whenXisoutsideBoard failed as board is not defined yet .

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
 
 public class TicTacToeGame {
-}
+
+    public static void main(String[] args) {
+
+    }
+    public static void printBoard(char[][] board) {
+
+    }
+    }
--- test/TicTacToeGameTest.java
+++ test/TicTacToeGameTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class TicTacToeGameTest {
+    private TicTacToeGame game;
+
+    @Before
+    public void setUp() {
+        this.game = new TicTacToeGame();
+    }
+
+
+    @Test(expected = RuntimeException.class)
+    public void whenXOutsideBoardException() {
+        game.printBoard(new char[4][2]);
+
+    }
 }
