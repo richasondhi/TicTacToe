a9f03b31e721572f83b8e9d4ba9b64f5597ea413
Richa Sondhi <richa.sondhi@ibm.com>
2021-11-15 14:43:51 +0100

    Implement code in order to make TEST givenLastTurnWasXWhenNextPlayerThenO passed to see whoes turn was it next

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -5,7 +5,7 @@ public class TicTacToeGame {
    static char[][] board = {{'\0', '\0', '\0'},
                       {'\0', '\0', '\0'}
                       , {'\0', '\0', '\0'}};
-
+     char lastPlayer = '\0';
     public static void main(String[] args) {
 
 
@@ -18,6 +18,7 @@ public class TicTacToeGame {
         checkAxis(x);
         checkAxis(y);
         setBox(x, y);
+        lastPlayer = nextPlayer();
     }
     private void checkAxis(int axis)
     {
@@ -34,11 +35,13 @@ public class TicTacToeGame {
             board[x - 1][y - 1] = 'X';
         }
     }
-    public char nextPlayer()
-    {
+
+    public char nextPlayer() {
+        if (lastPlayer == 'X') {
+            return 'O';
+        }
         return 'X';
     }
-
     public static void printBoard(char[][] board) {
         System.out.println(board[0][0] + "|" +  board[0][1] + "|" +  board[0][2] );
         System.out.println("-----");
